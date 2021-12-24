9b16d6cd877fe7ed8ca90db0aec55434b915ae52
bianchettihugo <lhbianchetti@gmail.com>
2021-12-24 16:34:21 -0300

    Challenge completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,26 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask : Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const index = tasks.findIndex(task => task.id === id);
+    if(index >= 0){
+      const taskList = tasks.map(task => ({ ... task }));
+      taskList[index].done = !taskList[index].done;
+      setTasks(taskList);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
